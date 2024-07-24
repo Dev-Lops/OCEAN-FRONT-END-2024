e4cf75e48f0385841cdc05b36d1ffeec419edf19
Anderson Lopes <developes.anderson@gmail.com>
2024-07-24 07:58:14 -0400

    🚀 Projeto Finalizado

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import './App.css'
+import { Card } from './components/Card/Card'
+
+export function App() {
+  const item1 = {
+    name: 'Rick Sanchez',
+    image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg'
+  }
+
+  const item2 = {
+    name: 'Morty Smith',
+    image: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg'
+  }
+
+  const item3 = {
+    name: 'Summer Smith',
+    image: 'https://rickandmortyapi.com/api/character/avatar/3.jpeg'
+  }
+
+  // Listas (Arrays)
+  const itens = [item1, item2, item3]
+
+  return (
+    <>
+      <div className="cards">
+        {/* Para cada um dos itens da lista, exibir um Card */}
+        {itens.map(elemento => <Card item={elemento} key={elemento.name} />)}
+      </div>
+    </>
+  )
+}
---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,19 @@
+
+
+export function Card(props) {
+	const item = props.item
+
+	return (
+		<div className="card">
+			<h2>{item.name}</h2>
+
+			<div className="tags">
+				<div>Status: Vivo</div>
+				<div>Espécie: Humana</div>
+				<div>Origem: Terra C-137</div>
+			</div>
+
+			<img src={item.image} />
+		</div>
+	)
+}
